057fb34d9489289014489b2eff9efcc0de3a75f8
Alan Camargo <alanhcamargo@gmail.com>
2020-05-07 03:26:54 -0300

    Add project

---
+++ solution/consumers/models/weather.py
@@ -0,0 +1,26 @@
+"""Contains functionality related to Weather"""
+import logging
+
+
+logger = logging.getLogger(__name__)
+
+
+class Weather:
+    """Defines the Weather model"""
+
+    def __init__(self):
+        """Creates the weather model"""
+        self.temperature = 70.0
+        self.status = "sunny"
+
+    def process_message(self, message):
+        """Handles incoming weather data"""
+        logger.info("weather process_message is incomplete - skipping")
+        # TODO: Process incoming weather messages. Set the temperature and status.
+        logger.debug("handling incoming weather message")
+        value = message.value()
+        self.temperature = value["temperature"]
+        self.status = value["status"]
+        logger.debug(
+            "weather is now %sf and %s", self.temperature, self.status.replace("_", " ")
+        )
